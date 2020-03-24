df33e706ee35c926679965f360c425ec72aa3464
Fernando <fernando.bragga@gmail.com>
2020-03-24 22:59:02 +1100

    added table action controls

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,10 +2,66 @@ class MembersController < ApplicationController
 
 
   def index
-    @members = Member.all.where(authorized_for_app: true)
+    @members = Member.all
+  end
+
+  def remove_from_app
+
+    @member = Member.find(params[:id])
+    @member.authorized_for_app = false
+
+    if @member.save
+
+      flash[:notice] = "User removed from the app successfully"
+      redirect_to members_path
+
+    else
+
+      flash[:alert] = "Something went wrong and the user has not been removed from the app"
+      redirect_to members_path
+
+    end
+
+  end # end remove_from_app
+
+  def edit
+    @member = Member.find(params[:id])
   end
 
 
+  def update
+
+    @member = Member.find(params[:id])
+
+    if @member.update(member_params)
+      flash[:notice] = "Member was successfully ipdated"
+      redirect_to members_path
+
+    else
+      flash[:alert] = "The update was not successful"
+      render 'edit'
+    end
+
+
+  end # end update
+
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    flash[:notice] = "The member was successfully deleted"
+    redirect_to members_path
+
+  end
+
   #@members = Member.all.where(authorized_for_app: false)
 
+
+  private
+
+  def member_params
+    params.require(:member).permit(:email, :membership_number, :first_name, :last_name, :authorized_for_app)
+  end
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :members
   root to: "news#index"
 
-  resources :members, only: [:index, :show]
+  resources :members
+
+  get 'remove_from_app/:id', to: 'members#remove_from_app', as: 'app_removal'
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
